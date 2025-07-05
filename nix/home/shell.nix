9f589ea1e1e2a626cfabbaca45fcec1434b406af
Freya Gaynor <freya@webwitch.org>
2025-07-05 15:15:22 -0700

    zsh and syntax highlighting

---
+++ nix/home/shell.nix
@@ -0,0 +1,20 @@
+{ pkgs, ... }:
+{
+  home.packages = with pkgs; [
+    starship
+    zsh
+    zoxide
+  ];
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+  };
+  programs.zsh.oh-my-zsh = {
+    enable = true;
+    plugins = [ "git" ];
+  };
+  programs.starship.enable = true;
+  programs.zoxide.enable = true;
+}
