585a22e9de936ded01d20f8b7cd3ae5f19a151be
Freya Gaynor <freya@webwitch.org>
2025-07-05 16:22:11 -0700

    simplify per-machine configs

--- nix/home/folkvangr.nix
+++ nix/home/folkvangr.nix
@@ -1,8 +1,6 @@
 { ... }:
 {
-  home = {
-    homeDirectory = "/home/witch";
-  };
+  home.homeDirectory = "/home/witch";
   programs.git.extraConfig = {
     core.sshCommand = "ssh.exe";
     gpg.ssh.program = "/mnt/c/Users/freya/AppData/Local/1Password/app/8/op-ssh-sign-wsl";
