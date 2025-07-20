5e7e0006c11a44b556eed50a7e55806593d3626c
Freya Gaynor <freya@webwitch.org>
2025-07-20 13:36:28 -0700

    vs-codium on osx

--- nix/home/1x1-osx.nix
+++ nix/home/1x1-osx.nix
@@ -2,6 +2,7 @@
 {
   imports = [
     ./common.nix
+    ./vs-codium.nix
   ];
   home.homeDirectory = "/Users/witch";
   programs.git.extraConfig.gpg.ssh.program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
