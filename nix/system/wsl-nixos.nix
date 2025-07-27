3b0a73e97dfb6d7037f4a019f17976ca136e574d
Freya Gaynor <freya@webwitch.org>
2025-07-26 19:51:03 -0700

    ssh on wsl-nixos

--- nix/system/wsl-nixos.nix
+++ nix/system/wsl-nixos.nix
@@ -9,4 +9,8 @@
   programs.nix-ld.enable = true;
   documentation.dev.enable = true;
   users.defaultUserShell = pkgs.zsh;
+  users.users.witch.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFPmZTMA6pUYhm8RxKRF6x7QMVGcueMnTrdOn1btnkRd"
+  ];
+  systemd.services.sshd.wantedBy = pkgs.lib.mkForce [ "multi-user.target" ];
 }
