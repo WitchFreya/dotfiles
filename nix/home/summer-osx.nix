064f3613d1f92c89ada7225e3ea106857c72f775
Freya Gaynor <freya@webwitch.org>
2025-08-25 08:15:44 -0700

    feat: configuration for summer laptop

---
+++ nix/home/summer-osx.nix
@@ -0,0 +1,12 @@
+{ ... }:
+{
+  imports = [
+    ./common.nix
+    ./vs-code
+  ];
+  home.homeDirectory = "/Users/witch";
+  programs.git.extraConfig.gpg.ssh.program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+  programs.zsh.shellAliases = {
+    upgrade = "sudo darwin-rebuild switch --flake ~/Documents/dotfiles";
+  };
+}
