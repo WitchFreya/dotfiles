d8eeb4d00b9783de22b4f24737ff3dddb7c6e5fc
Freya Gaynor <freya@webwitch.org>
2025-08-25 10:30:12 -0700

    fix(osx): Remove emulator portion

--- nix/system/nix-darwin.nix
+++ nix/system/nix-darwin.nix
@@ -8,15 +8,6 @@
   system.stateVersion = 6;
   # allow sudo auth with touch id
   security.pam.services.sudo_local.touchIdAuth = true;
-  nix.linux-builder = {
-    enable = true;
-    systems = [
-      "aarch64-linux"
-      "x86_64-linux"
-    ];
-    ephemeral = true;
-    config.boot.binfmt.emulatedSystems = [ "x86_64-linux" ];
-  };
 
   users.users.witch = {
     home = "/Users/witch";
