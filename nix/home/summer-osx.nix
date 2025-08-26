c9f0dc148eb22c5a3eedf47906c59acb0b710e9a
Freya Gaynor <freya@webwitch.org>
2025-08-26 11:27:37 -0700

    feat(osx): ssh identity agent

--- nix/home/summer-osx.nix
+++ nix/home/summer-osx.nix
@@ -10,4 +10,13 @@
   programs.zsh.shellAliases = {
     upgrade = "sudo darwin-rebuild switch --flake ~/Documents/vcs/dotfiles";
   };
+  programs.ssh =
+    let
+      onePassPath = "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+      IdentityAgent = ''IdentityAgent "${onePassPath}"'';
+    in
+    {
+      enable = true;
+      extraConfig = IdentityAgent;
+    };
 }
