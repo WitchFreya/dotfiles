0fb6a1c71e7d5a906a0a5b6e91f9033f9ef64102
Freya Gaynor <freya@webwitch.org>
2025-08-26 08:09:36 -0700

    fix(osx): Correct the upgrade alias

--- nix/home/summer-osx.nix
+++ nix/home/summer-osx.nix
@@ -7,6 +7,6 @@
   home.homeDirectory = "/Users/witch";
   programs.git.extraConfig.gpg.ssh.program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
   programs.zsh.shellAliases = {
-    upgrade = "sudo darwin-rebuild switch --flake ~/Documents/dotfiles";
+    upgrade = "sudo darwin-rebuild switch --flake ~/Documents/vcs/dotfiles";
   };
 }
