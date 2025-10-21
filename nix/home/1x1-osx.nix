17c96e71f2e23516db548441338bb33f938d624c
Freya Gaynor <freya@webwitch.org>
2025-10-21 08:28:42 -0700

    simplify per-machine configs

--- nix/home/1x1-osx.nix
+++ nix/home/1x1-osx.nix
@@ -1,11 +1,7 @@
 { ... }:
 {
-  home = {
-    homeDirectory = "/Users/witch";
-  };
-  programs.git.extraConfig = {
-    gpg.ssh.program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
-  };
+  home.homeDirectory = "/Users/witch";
+  programs.git.extraConfig.gpg.ssh.program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
   programs.zsh.shellAliases = {
     upgrade = "sudo darwin-rebuild switch --flake ~/vcs/nix-config";
     update = "home-manager switch --flake ~/vcs/nix-config";
