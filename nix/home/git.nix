6a22086a197a24158a3b5e1dc6ee048118e7011d
Freya Gaynor <freya@webwitch.org>
2025-10-21 08:28:42 -0700

    better git configuration

--- nix/home/git.nix
+++ nix/home/git.nix
@@ -1,10 +1,17 @@
-{ ... }:
+{ pkgs, ... }:
+let
+  key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFPmZTMA6pUYhm8RxKRF6x7QMVGcueMnTrdOn1btnkRd";
+  email = "freya@webwitch.org";
+  signersFile = pkgs.writeText "git-allowed-signers" ''
+    ${email} namespaces="git" ${key}
+  '';
+in
 {
   programs.git = {
     enable = true;
     userName = "Freya Gaynor";
-    userEmail = "freya@webwitch.org";
-    signing.key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFPmZTMA6pUYhm8RxKRF6x7QMVGcueMnTrdOn1btnkRd";
+    userEmail = email;
+    signing.key = key;
     signing.signByDefault = true;
     signing.format = "ssh";
 
@@ -12,6 +19,8 @@
       init.defaultBranch = "main";
       core.editor = "code --wait";
       pull.rebase = true;
+
+      gpg.ssh.allowedSignersFile = builtins.toString signersFile;
     };
   };
 }
