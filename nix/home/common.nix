d9c49a48739e58d618b8b631a7c0a96fae0b8710
Freya Gaynor <freya@webwitch.org>
2025-10-21 08:28:42 -0700

    zsh and syntax highlighting

--- nix/home/common.nix
+++ nix/home/common.nix
@@ -1,27 +1,11 @@
-{ pkgs, ... }:
+{ ... }:
 {
   imports = [
+    ./shell.nix
     ./git.nix
   ];
   home = {
     username = "witch";
     stateVersion = "25.11";
-    packages = with pkgs; [
-      starship
-      zsh
-      zoxide
-    ];
   };
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    autosuggestion.enable = true;
-
-    oh-my-zsh = {
-      enable = true;
-      plugins = [ "git" ];
-    };
-  };
-  programs.starship.enable = true;
-  programs.zoxide.enable = true;
 }
