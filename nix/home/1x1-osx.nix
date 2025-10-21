9d7463b0ee2d8e4038c4ea748b67f6747d76e64b
Freya Gaynor <freya@webwitch.org>
2025-10-21 08:28:41 -0700

    upgrade and update aliases

--- nix/home/1x1-osx.nix
+++ nix/home/1x1-osx.nix
@@ -6,4 +6,8 @@
   programs.git.extraConfig = {
     "gpg \"ssh\"".program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
   };
+  programs.zsh.shellAliases = {
+    upgrade = "sudo darwin-rebuild switch --flake ~/vcs/nix-config";
+    update = "home-manager switch --flake ~/vcs/nix-config";
+  };
 }
--- nix/home/folkvangr.nix
+++ nix/home/folkvangr.nix
@@ -7,4 +7,9 @@
     core.sshCommand = "ssh.exe";
     "gpg \"ssh\"".program = "/mnt/c/Users/freya/AppData/Local/1Password/app/8/op-ssh-sign-wsl";
   };
+
+  programs.zsh.shellAliases = {
+    upgrade = "sudo nixos-rebuild switch --flake ~/vcs/nix-config";
+    update = "home-manager switch --flake ~/vcs/nix-config";
+  };
 }
