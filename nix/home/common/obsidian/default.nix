b66b15b1a6d7c403fb2f037840d271ecfc51759f
Freya Gaynor <freya@webwitch.org>
2025-10-21 08:29:07 -0700

    feat(obsidian): explicit core plugins

--- nix/home/common/obsidian/default.nix
+++ nix/home/common/obsidian/default.nix
@@ -5,6 +5,27 @@
     vaults."obsidian" = {
       enable = true;
       target = "vcs/obsidian";
+      settings.corePlugins = [
+        "backlink"
+        "bookmarks"
+        "canvas"
+        "command-palette"
+        "daily-notes"
+        "editor-status"
+        "file-explorer"
+        "file-recovery"
+        "global-search"
+        "graph"
+        "note-composer"
+        "outgoing-link"
+        "outline"
+        "page-preview"
+        "switcher"
+        "tag-pane"
+        "templates"
+        "word-count"
+        "workspaces"
+      ];
       settings.communityPlugins = [
         {
           pkg = pkgs.callPackage ./plugins/obsidian-git.nix { };
