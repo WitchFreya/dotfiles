2ff0c5142f94ff567c8f63e34e7e3dd4436ce0fa
Freya Gaynor <freya@webwitch.org>
2025-10-21 08:28:46 -0700

    vscode settings improvements and missing packages

--- nix/home/vs-codium.nix
+++ nix/home/vs-codium.nix
@@ -7,27 +7,32 @@
       enableUpdateCheck = false;
       enableExtensionUpdateCheck = false;
       userSettings = builtins.fromJSON (builtins.readFile ../../.vscode/settings.json);
-      extensions = pkgs.nix4vscode.forVscode [
-        # visuals
-        "akamud.vscode-theme-onedark"
-        "pkief.material-icon-theme"
+      extensions =
+        pkgs.nix4vscode.forVscode [
+          # visuals
+          "akamud.vscode-theme-onedark"
+          "pkief.material-icon-theme"
 
-        # basic features
-        "mkhl.direnv"
-        "streetsidesoftware.code-spell-checker"
-        "wmaurer.change-case"
-        "firefox-devtools.vscode-firefox-debug"
-        "fill-labs.dependi"
-        "editorconfig.editorconfig"
-        "esbenp.prettier-vscode"
-        "ue.alphabetical-sorter"
+          # basic features
+          "mkhl.direnv"
+          "streetsidesoftware.code-spell-checker"
+          "wmaurer.change-case"
+          "firefox-devtools.vscode-firefox-debug"
+          "fill-labs.dependi"
+          "editorconfig.editorconfig"
+          "esbenp.prettier-vscode"
+          "ue.alphabetical-sorter"
+          "gruntfuggly.todo-tree"
+          "eamodio.gitlens"
 
-        # language-specific
-        "tamasfe.even-better-toml"
-        "dbaeumer.vscode-eslint"
-        "jnoortheen.nix-ide"
-        "yoavbls.pretty-ts-errors"
-      ];
+          # language-specific
+          "tamasfe.even-better-toml"
+          "dbaeumer.vscode-eslint"
+          "yoavbls.pretty-ts-errors"
+        ]
+        ++ pkgs.nix4vscode.forOpenVsx [
+          "jnoortheen.nix-ide"
+        ];
     };
   };
 }
