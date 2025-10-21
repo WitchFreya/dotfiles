288dfd95da2b452147f9efa400972fe58d7df7d3
Freya Gaynor <freya@webwitch.org>
2025-10-21 08:28:56 -0700

    feat: custom git module

--- nix/home/common.nix
+++ nix/home/common.nix
@@ -1,12 +1,14 @@
-{ ... }:
+{ lib, ... }:
 {
   imports = [
+    ../modules/git.nix
     ./shell.nix
-    ./git.nix
     ./nix-direnv.nix
   ];
   home = {
     username = "witch";
     stateVersion = "25.11";
   };
+  git.email = lib.mkDefault "freya@webwitch.org";
+  git.publicKey = lib.mkDefault "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFPmZTMA6pUYhm8RxKRF6x7QMVGcueMnTrdOn1btnkRd";
 }
