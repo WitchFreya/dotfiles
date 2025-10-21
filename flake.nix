7398cf650b860eae4b5c1927d14874eb79dfbe4a
Freya Gaynor <freya@webwitch.org>
2025-10-21 08:28:40 -0700

    feat: darwin nix home-manager setup

--- flake.nix
+++ flake.nix
@@ -23,7 +23,16 @@
         "witch@Folkvangr" = home-manager.lib.homeManagerConfiguration {
           pkgs = nixpkgs.legacyPackages.x86_64-linux;
           modules = [
+            ./nix/home/shared.nix
             ./nix/home/folkvangr.nix
+          ];
+        };
+
+        "witch@1x1-osx" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+          modules = [
+            ./nix/home/shared.nix
+            ./nix/home/1x1-osx.nix
           ];
         };
       };
