c7ebc1cad2234262750aa0d35a5dd7ea2945e850
Freya Gaynor <freya@webwitch.org>
2025-12-05 08:19:31 -0800

    fix(relay): Use tag instead of rev

--- nix/home/common/obsidian/plugins/relay.nix
+++ nix/home/common/obsidian/plugins/relay.nix
@@ -6,12 +6,12 @@ pkgs.buildNpmPackage (finalAttrs: {
   src = pkgs.fetchgit {
     name = finalAttrs.pname;
     url = "https://github.com/No-Instructions/Relay.git";
-    hash = "sha256-8L7OyMxgfJwKHt6tJ3eM6YR8iuFvnO8ciRowBIrGgoo=";
+    hash = "sha256-n1WnXJXeLqKq6WmTZKU2c1nZ6IZ4IK01gv1oDdZlyNA=";
     leaveDotGit = true;
-    rev = finalAttrs.version;
+    tag = finalAttrs.version;
   };
 
-  npmDepsHash = "sha256-n/jGuzvKQ0ypKN5iy8B3O7mEOkIgAPZCh2oMEQ071ek=";
+  npmDepsHash = "sha256-w4IvtFxWcQr1noHTL9HuR3Q5COfG/9EnqKjLDCq4TW8=";
 
   nativeBuildInputs = with pkgs; [ git ];
 
