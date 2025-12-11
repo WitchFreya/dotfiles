f3ddf301f9da160918027a8460197f11a8c0ede1
Freya Gaynor <freya@webwitch.org>
2025-12-11 08:35:06 -0800

    feat(obsidian/relay): Bump version to 0.7.1

--- nix/home/common/obsidian/plugins/relay.nix
+++ nix/home/common/obsidian/plugins/relay.nix
@@ -1,12 +1,12 @@
 { pkgs, ... }:
 pkgs.buildNpmPackage (finalAttrs: {
   pname = "obsidian.plugins.relay";
-  version = "0.7.0";
+  version = "0.7.1";
 
   src = pkgs.fetchgit {
     name = finalAttrs.pname;
     url = "https://github.com/No-Instructions/Relay.git";
-    hash = "sha256-yJ1KYiMvYg5KaRmkFadv5xyVEnPxoW+KELiEyNfw0os=";
+    hash = "sha256-/OGW32zwCI9ApMQ9CgenX2NwahXKUSlTOHSZx4Nj6cw=";
     leaveDotGit = true;
     tag = finalAttrs.version;
   };
