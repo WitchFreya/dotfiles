32843f891b0013fe5b8fd3e85979916d257c8b65
Freya Gaynor <freya@webwitch.org>
2026-04-20 09:19:09 -0700

    feat(obsidian/git): Bump version to v2.32.2

--- nix/home/common/obsidian/plugins/obsidian-git.nix
+++ nix/home/common/obsidian/plugins/obsidian-git.nix
@@ -1,19 +1,19 @@
 { pkgs, ... }:
 pkgs.stdenv.mkDerivation (finalAttrs: {
   pname = "obsidian.plugins.obsidian-git";
-  version = "2.38.0";
+  version = "2.38.2";
 
   src = pkgs.fetchFromGitHub {
     owner = "Vinzent03";
     repo = "obsidian-git";
     rev = finalAttrs.version;
-    hash = "sha256-cG7TBoLFUbsOHd/H2O+iRTmxj5lkPh3u0mNo8lCsiuM=";
+    hash = "sha256-bwZTAFbHtNRHes00L598OCwHQufrxnwkGlKFIbSljH8=";
   };
 
   pnpmDeps = pkgs.fetchPnpmDeps {
     inherit (finalAttrs) pname version src;
     fetcherVersion = 2;
-    hash = "sha256-1oMTNk/3kDQQGecBGu1m4ECktJvdm3f0AD3lWdBfCP4=";
+    hash = "sha256-M9uFT6+HCthPbV9PKnieY/7bKQR/BypelI+7taHhCbM=";
   };
 
   nativeBuildInputs = with pkgs; [
