77540c350604a0b5facadddafdce58d490d11030
Freya Gaynor <freya@webwitch.org>
2026-05-15 07:59:59 -0700

    deps(obsidian-git): Bump to v2.38.3

--- nix/home/common/obsidian/plugins/obsidian-git.nix
+++ nix/home/common/obsidian/plugins/obsidian-git.nix
@@ -1,19 +1,19 @@
 { pkgs, ... }:
 pkgs.stdenv.mkDerivation (finalAttrs: {
   pname = "obsidian.plugins.obsidian-git";
-  version = "2.38.2";
+  version = "2.38.3";
 
   src = pkgs.fetchFromGitHub {
     owner = "Vinzent03";
     repo = "obsidian-git";
     rev = finalAttrs.version;
-    hash = "sha256-bwZTAFbHtNRHes00L598OCwHQufrxnwkGlKFIbSljH8=";
+    hash = "sha256-LcFIpQHOLjCY4L2vCYBjBN/73Bfg5Wa+tXoVmcBMrbY=";
   };
 
   pnpmDeps = pkgs.fetchPnpmDeps {
     inherit (finalAttrs) pname version src;
     fetcherVersion = 2;
-    hash = "sha256-M9uFT6+HCthPbV9PKnieY/7bKQR/BypelI+7taHhCbM=";
+    hash = "sha256-dMl6SXEuZlEIm1jOR6gI5hBBk236FItxga2cx2d8D0g=";
   };
 
   nativeBuildInputs = with pkgs; [
