6dec9ce83e4705c4ed636d38de8949a35ee10391
Freya Gaynor <freya@webwitch.org>
2026-05-19 08:27:21 -0700

    deps(relay): bump relay to v0.8.0

--- nix/home/common/obsidian/plugins/relay.nix
+++ nix/home/common/obsidian/plugins/relay.nix
@@ -1,17 +1,17 @@
 { pkgs, ... }:
 pkgs.buildNpmPackage (finalAttrs: {
   pname = "obsidian.plugins.relay";
-  version = "0.7.4";
+  version = "0.8.0";
 
   src = pkgs.fetchgit {
     name = finalAttrs.pname;
     url = "https://github.com/No-Instructions/Relay.git";
-    hash = "sha256-KTEB2OsYhlrbuqIckmdjuVZ0TKdXLlPRgTSf51Eq2zk=";
+    hash = "sha256-4+MbrXBnkFgj0PNZr5BN0jILM1KhIl+kQQpVTySyf30=";
     leaveDotGit = true;
     rev = finalAttrs.version;
   };
 
-  npmDepsHash = "sha256-00H9wDu7eMWTbMvwNTNCP3/0dzncPRRXF7Dyubf58Z4=";
+  npmDepsHash = "sha256-/1MmrZRzVyAOPVhcc5dB/zTikMh/BwI4Lm9cVyJFhwo=";
 
   nativeBuildInputs = with pkgs; [
     git
